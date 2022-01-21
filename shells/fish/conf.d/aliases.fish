7de94dee07cf3e0aa2e2d25596cbb2435d07f241
Aiko Wessels <aiko.wessels@gmail.com>
2022-01-21 14:18:14 +0100

    Update exa aliases for zsh and fish

--- shells/fish/conf.d/aliases.fish
+++ shells/fish/conf.d/aliases.fish
@@ -1,7 +1,16 @@
 # Commands to run in interactive sessions can go here
-alias ls='exa -F'
-alias ll='exa -Fla'
-alias lt='exa --tree'
+if command -v exa &>/dev/null
+  alias l='exa'
+  alias ls='exa -F'
+  alias ll='exa -Fl'
+  alias lg='exa -l --git'
+  alias lt='exa --tree'
+  alias lll='exa -Fla'
+else
+  alias l='ls'
+  alias ll='ls -Al'
+end
+
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
