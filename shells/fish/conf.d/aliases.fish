ff573bd88ac46d699ab20ccf05a14016ed7b30de
Aiko Wessels <aiko.wessels@gmail.com>
2022-01-26 09:41:45 +0100

    Update exa aliases

--- shells/fish/conf.d/aliases.fish
+++ shells/fish/conf.d/aliases.fish
@@ -1,11 +1,11 @@
 # Commands to run in interactive sessions can go here
 if command -v exa &>/dev/null
   alias l='exa'
-  alias ls='exa -F'
-  alias ll='exa -Fl'
-  alias lg='exa -l --git'
-  alias lt='exa --tree'
-  alias lll='exa -Fla'
+  alias ls='exa --group-directories-first --icons'
+  alias lg='exa -l --group-directories-first --icons --git'
+  alias ll='exa -l --group-directories-first --icons'
+  alias lt='exa --group-directories-first --tree --level 2 --icons'
+  alias lll='exa -la --group-directories-first --icons --group'
 else
   alias l='ls'
   alias ll='ls -Al'
