2a4ca9d719b3a79fbd47c59672378afa2ef3a4e8
Aiko Wessels <aiko.wessels@gmail.com>
2022-02-02 20:31:03 +0100

    Fix bugs in init.lua and plugins.lua

--- tools/nvim/lua/plugins.lua
+++ tools/nvim/lua/plugins.lua
@@ -89,35 +89,6 @@ plugins = require('packer').startup(function(use)
       requires = { 'nvim-lua/plenary.nvim' },
       config = function()
         require('gitsigns').setup{
-          on_attach = function(bufnr)
-            local gs = package.loaded.gitsigns
-
-            local function map(mode, l, r, opts)
-              opts = opts or {}
-              opts.buffer = bufnr
-              vim.keymap.set(mode, l, r, opts)
-            end
-
-            -- Navigation
-            map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
-            map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
-
-            -- Git actions related actions with <leader>g...
-            map({'n', 'v'}, '<leader>gs', ':Gitsigns stage_hunk<CR>')
-            map({'n', 'v'}, '<leader>gr', ':Gitsigns reset_hunk<CR>')
-            map('n', '<leader>gS', gs.stage_buffer)
-            map('n', '<leader>gu', gs.undo_stage_hunk)
-            map('n', '<leader>gR', gs.reset_buffer)
-            map('n', '<leader>gp', gs.preview_hunk)
-            map('n', '<leader>gl', function() gs.blame_line{full=true} end)
-            map('n', '<leader>gL', gs.toggle_current_line_blame)
-            map('n', '<leader>gd', gs.diffthis)
-            map('n', '<leader>gD', function() gs.diffthis('~') end)
-            map('n', '<leader>gtd', gs.toggle_deleted)
-
-            -- Text object
-            map({'o', 'x'}, 'ig', ':<C-U>Gitsigns select_hunk<CR>')
-          end
         }
       end
     }
