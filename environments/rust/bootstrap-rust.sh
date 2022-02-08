d3112c19bd9963d8a6fcf925a876515670f88fbb
Aiko Wessels <aiko.wessels@gmail.com>
2022-02-08 07:34:30 +0100

    Fix full config files for dotbot

--- environments/rust/bootstrap-rust.sh
+++ environments/rust/bootstrap-rust.sh
@@ -1,5 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
 readonly CARGO_HOME=~/.cargo
 
+# Rust compile is already installed.
+if command -v rustc &>/dev/null; then
+  exit 0
+fi
+
+# If the directory does not exist, run the installer.
 if [[ ! -d "${CARGO_HOME}" ]]; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
--- tools/fzf/bootstrap-fzf.sh
+++ tools/fzf/bootstrap-fzf.sh
@@ -1,5 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Check if FZF is already installed.
+if command -v fzf &>/dev/null; then
+  exit 0
+fi
+
+# If the FZF directory does not exist, then clone the rpeository and run the installer.
 if [[ ! -d ~/.fzf ]]; then
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 fi
