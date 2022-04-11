99dfb2b14395dcb6974ddacfb9a82e9f8eb2e588
Aiko Wessels <aiko.wessels@gmail.com>
2022-04-11 20:47:35 +0200

    fix(fish): commandline_append now adds all lines

--- shells/fish/functions/commandline_append.fish
+++ shells/fish/functions/commandline_append.fish
@@ -1,8 +1,9 @@
 # Deciphered from fzf-file-widget. Somewhat unclear why it doesn't exist already!
 function commandline_append -d 'add stdin to the command line, for fzf functions'
-  read -l result
-  commandline -t ""
-  commandline -it -- (string escape $result)
+  while read -l result
+    commandline -t ""
+    commandline -it -- (string escape $result)" "
+  end
   commandline -f repaint
 end
 
