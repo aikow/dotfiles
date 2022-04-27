3dab59d1c174ccf4a281d65ee4e1f3b21a4d233b
Aiko Wessels <aiko.wessels@gmail.com>
2022-04-27 07:56:06 +0200

    fix(nvim): add missing quotes to py file format

--- tools/nvim/after/ftplugin/python.lua
+++ tools/nvim/after/ftplugin/python.lua
@@ -4,5 +4,5 @@ vim.opt_local.smarttab = true
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
-vim.opt_local.fileformat = unix
+vim.opt_local.fileformat = "unix"
 vim.opt_local.textwidth = 80
