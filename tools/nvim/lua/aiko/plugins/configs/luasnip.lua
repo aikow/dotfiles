0ac9b407b83d5a89bea53df2bdf755d5b3dd990c
Aiko Wessels <aiko.wessels@gmail.com>
2022-06-02 08:26:54 +0200

    feat(nvim): fix triggers for cmp and luasnip

--- tools/nvim/lua/aiko/plugins/configs/luasnip.lua
+++ tools/nvim/lua/aiko/plugins/configs/luasnip.lua
@@ -1,16 +1,33 @@
 local M = {}
 
 M.setup = function()
-  local luasnip = require("luasnip")
+	local map = vim.keymap.set
+	local luasnip = require("luasnip")
 
-  luasnip.config.set_config({
-    history = true,
-    updateevents = "TextChanged,TextChangedI",
-  })
+	luasnip.config.set_config({
+		history = true,
+		updateevents = "TextChanged,TextChangedI",
+	})
 
-  require("luasnip.loaders.from_vscode").lazy_load()
+	map({ "i" }, "<Tab>", "<Plug>luasnip-expand-or-jump", { silent = true, expr = true })
+	map("s", "<Tab>", function()
+		require("luasnip").jump(1)
+	end, { silent = true })
+	map("i", "<S-Tab>", function()
+		require("luasnip").jump(-1)
+	end, { silent = true })
+	map("s", "<S-Tab>", function()
+		require("luasnip").jump(-1)
+	end, { silent = true })
 
-  print("Luasnip")
+	map(
+		{ "i", "s" },
+		"<C-s>",
+		[[luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>']],
+		{ silent = true, expr = true }
+	)
+
+	require("luasnip.loaders.from_vscode").lazy_load()
 end
 
 return M
