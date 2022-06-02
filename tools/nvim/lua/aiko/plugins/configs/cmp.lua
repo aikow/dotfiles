a21ed427e0e598197c4e57d17a517fc668aba283
Aiko Wessels <aiko.wessels@gmail.com>
2022-06-02 09:37:39 +0200

    feat(nvim): add custom snippet structure

--- tools/nvim/lua/aiko/plugins/configs/cmp.lua
+++ tools/nvim/lua/aiko/plugins/configs/cmp.lua
@@ -98,6 +98,25 @@ M.setup = function()
   -- Use cmdline & path source for ":"
   cmp.setup.cmdline(":", {
     mapping = {
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif require("luasnip").expand_or_jumpable() then
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+        else
+          fallback()
+        end
+      end, { "c" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif require("luasnip").jumpable(-1) then
+          vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+        else
+          fallback()
+        end
+      end, { "c" }),
+
       ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "c" }),
       ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "c" }),
 
