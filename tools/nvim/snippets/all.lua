c897893c30e22faf3c6799d604ae302493706447
Aiko Wessels <aiko.wessels@gmail.com>
2022-06-09 17:21:08 +0200

    feat(nvim): cleanup all box snippets with comments

--- tools/nvim/snippets/all.lua
+++ tools/nvim/snippets/all.lua
@@ -15,86 +15,148 @@ local m = require("luasnip.extras").m
 local lambda = require("luasnip.extras").l
 
 local replace = function(index, char)
-  return f(function(arg)
-    return string.rep(char, string.len(arg[1][1]))
-  end, { index })
+	return f(function(arg)
+		return string.rep(char, string.len(arg[1][1]))
+	end, { index })
 end
 
-local comment = function()
-  return f(function()
-    local cs = vim.o.commentstring
-    local parts = vim.split(cs, "%s", true)
-    local start, ending = parts[1], parts[2]
-    return start
-  end, {})
+local parse_comments = function()
+	-- Parse vim 'comments' option to extract line comment format.
+	-- See the help for the exact syntax.
+	local iter = vim.gsplit(vim.o.comments, ",", true)
+	local comments = { single = {}, triple = {}, other = {} }
+
+	local split = function(cs)
+		local flags, text = string.match(cs or "", "^([^:]*):([^:]*)$")
+    return flags or "", text or ""
+	end
+
+	while true do
+    local cs = iter()
+    if not cs then break end
+		local flags, text = split(cs)
+
+		if #flags == 0 then
+			-- Parse the single line comment string.
+			table.insert(comments.other, 1, { start = text, mid = text, stop = text, indent = "" })
+		elseif flags:match("s") and not flags:match("O") then
+			-- Parse 3 part comment string, but ignore those with the O flag
+			local ctriple = {}
+			local indent = ""
+
+			if string.match(flags:sub(-1), "%d+") then
+				indent = string.rep(" ", flags:sub(-1))
+			end
+			ctriple.start = text
+
+      cs = iter()
+      flags, text = split(cs)
+			assert(vim.startswith(flags, "m"), "Expected middle comment format of triple")
+			ctriple.mid = text
+
+      cs = iter()
+      flags, text = split(cs)
+			assert(vim.startswith(flags, "e"), "Expected end comment format of triple")
+			ctriple.stop = text
+			ctriple.indent = indent
+
+			table.insert(comments.triple, 1, ctriple)
+		elseif flags:match("b") then
+			if #text == 1 then
+				table.insert(comments.single, 1, { start = text, mid = text, stop = text, indent = "" })
+			end
+		end
+	end
+
+  return comments
+end
+
+local comment_format = function()
+
+	if vim.o.commentstring:match("%%s$") then
+		-- Remove last two characters
+    local cf
+		cf = vim.trim(vim.o.commentstring:sub(1, -3))
+		return { start=cf, mid=cf, stop=cf, indent="" }
+	end
+
+	local comments = parse_comments()
+  if not vim.tbl_isempty(comments.single) then
+    return comments.single[1]
+  end
+
+  if not vim.tbl_isempty(comments.other) then
+    return comments.other[1]
+  end
+
+  return comments.triple[1]
+end
+
+local comment = function(part)
+	return f(function()
+    local cf = comment_format()
+    if part == "mid" then
+      return cf.indent .. cf[part]
+    else
+      return cf[part]
+    end
+	end, {})
 end
 
 return {
-  s(
-    "lsep",
-    fmt(
-      [[
-        {2} ------------------------------------------------------------------------
-        {2} | {1}
-        {2} ------------------------------------------------------------------------
+	s(
+		"ubox",
+		fmt(
+			[[
+        {3} ┌─{2}─┐
+        {4} │ {1} │
+        {5} └─{2}─┘
       ]],
-      { i(1), comment() }
-    )
-  ),
+			{ i(1), replace(1, "─"), comment("start"), comment("mid"), comment("stop")  }
+		)
+	),
 
-  s(
-    "ubox",
-    fmt(
-      [[
-      {3} ┌─{2}─┐
-      {3} │ {1} │
-      {3} └─{2}─┘
+	s(
+		"uubox",
+		fmt(
+			[[
+        {4} ┌────{2}────┐
+        {5} │┌───{2}───┐│
+        {5} ││   {3}   ││
+        {5} ││   {1}   ││
+        {5} ││   {3}   ││
+        {5} │└───{2}───┘│
+        {6} └────{2}────┘
       ]],
-      { i(1), replace(1, "─"), comment() }
-    )
-  ),
+			{ i(1), replace(1, "─"), replace(1, " "), comment("start"), comment("mid"), comment("stop")  }
+		)
+	),
 
-  s(
-    "uubox",
-    fmt(
-      [[
-      {4} ┌────{2}────┐
-      {4} │┌───{2}───┐│
-      {4} ││   {3}   ││
-      {4} ││   {1}   ││
-      {4} ││   {3}   ││
-      {4} │└───{2}───┘│
-      {4} └────{2}────┘
-      ]],
-      { i(1), replace(1, "─"), replace(1, " "), comment() }
-    )
-  ),
-
-  s(
-    "box",
-    fmt(
-      [[
+	s(
+		"box",
+		fmt(
+			[[
         {3} ----{2}----
-        {3} |   {1}   |
-        {3} ----{2}----
+        {4} |   {1}   |
+        {5} ----{2}----
       ]],
-      { i(1), replace(1, "-"), comment() }
-    )
-  ),
+			{ i(1), replace(1, "-"), comment("start"), comment("mid"), comment("stop")  }
+		)
+	),
 
-  s(
-    "bbox",
-    fmt(
-      [[
+	s(
+		"bbox",
+		fmt(
+			[[
         {4} ====={2}=====
-        {4} |===={2}====|
-        {4} ||   {3}   ||
-        {4} ||   {1}   ||
-        {4} ||   {3}   ||
-        {4} |===={2}====|
-        {4} ====={2}=====
+        {5} |===={2}====|
+        {5} ||   {3}   ||
+        {5} ||   {1}   ||
+        {5} ||   {3}   ||
+        {5} |===={2}====|
+        {6} ====={2}=====
       ]],
-      { i(1), replace(1, "="), replace(1, " "), comment() }
-    )
-  ),
+			{ i(1), replace(1, "="), replace(1, " "), comment("start"), comment("mid"), comment("stop") }
+		)
+	),
 }
--- tools/nvim/snippets/tex/greek.lua
+++ tools/nvim/snippets/tex/greek.lua
@@ -11,28 +11,28 @@ local fmt = require("luasnip.extras.fmt").fmt
 -- ----------------------
 
 local math = function()
-	return vim.fn["vimtex#syntax#in_mathzone"]() == 1
+  return vim.fn["vimtex#syntax#in_mathzone"]() == 1
 end
 
 local notmath = function()
-	return not math()
+  return not math()
 end
 
 local comment = function()
-	return vim.fn["vimtex#syntax_in_comment"]() == 1
+  return vim.fn["vimtex#syntax_in_comment"]() == 1
 end
 
 local notcomment = function()
-	return not comment()
+  return not comment()
 end
 
 local env = function(name)
-	local bounds = vim.fn["vimtex#env#is_inside"](name)
-	return bounds[1] ~= 0 and bounds[2] ~= 0
+  local bounds = vim.fn["vimtex#env#is_inside"](name)
+  return bounds[1] ~= 0 and bounds[2] ~= 0
 end
 
 local notenv = function(name)
-	return not env(name)
+  return not env(name)
 end
 
 -- ----------------
@@ -40,20 +40,20 @@ end
 -- ----------------
 
 local context = {
-	math = function(callbacks)
-		return {
-			condition = math,
-			show_condition = math,
-			callbacks = callbacks,
-		}
-	end,
-	notmath = function(callbacks)
-		return {
-			condition = notmath,
-			show_condition = notmath,
-			callbacks = callbacks,
-		}
-	end,
+  math = function(callbacks)
+    return {
+      condition = math,
+      show_condition = math,
+      callbacks = callbacks,
+    }
+  end,
+  notmath = function(callbacks)
+    return {
+      condition = notmath,
+      show_condition = notmath,
+      callbacks = callbacks,
+    }
+  end,
 }
 
 -- ------------------------
@@ -61,20 +61,20 @@ local context = {
 -- ------------------------
 
 local autoinsert_space = {
-	-- index `-1` means the callback is on the snippet as a whole
-	[-1] = {
-		[events.leave] = function()
-			vim.api.nvim_create_autocmd("InsertCharPre", {
-				buffer = 0,
-				once = true,
-				callback = function()
-					if string.find(vim.v.char, "%a") then
-						vim.v.char = " " .. vim.v.char
-					end
-				end,
-			})
-		end,
-	},
+  -- index `-1` means the callback is on the snippet as a whole
+  [-1] = {
+    [events.leave] = function()
+      vim.api.nvim_create_autocmd("InsertCharPre", {
+        buffer = 0,
+        once = true,
+        callback = function()
+          if string.find(vim.v.char, "%a") then
+            vim.v.char = " " .. vim.v.char
+          end
+        end,
+      })
+    end,
+  },
 }
 
 -- ----------------
@@ -82,78 +82,92 @@ local autoinsert_space = {
 -- ----------------
 
 return {},
-	{
-		-- Display math
-		s({ trig = "mk", priority = 100 }, fmt([[${1}$]], { i(1) }), context.notmath(autoinsert_space)),
+  {
+    -- Display math
+    s({ trig = "mk", priority = 100 }, fmt([[${1}$]], { i(1) }), context.notmath(autoinsert_space)),
 
-		s(
-			{ trig = "([%w_]+)mk", regTrig = true, priority = 200 },
-			f(function(_, snip)
-				return "$" .. snip.captures[1] .. "$"
-			end),
-			context.notmath(autoinsert_space)
-		),
+    s(
+      { trig = "([%w_]+)mk", regTrig = true, priority = 200 },
+      f(function(_, snip)
+        return "$" .. snip.captures[1] .. "$"
+      end),
+      context.notmath(autoinsert_space)
+    ),
 
-		-- Inline math
-		s(
-			"dm",
-			fmt(
-				[[
+    -- Inline math
+    s(
+      "dm",
+      fmt(
+        [[
           \[
             {1}
           \]
         ]],
-				i(1)
-			),
-			context.notmath()
-		),
+        i(1)
+      ),
+      context.notmath()
+    ),
 
-		-- Fractions
-		s({ trig = "//", wordTrig = false }, { t([[\frac{]]), i(1), t([[}{]]), i(2), t([[}]]) }, context.math()),
-		s(
-			{ trig = [=[([a-zA-Z0-9_^\{}]+)/]=], wordTrig = false, regTrig = true },
-			{ t([[\frac{]]), f(function(_, snip)
-				return snip.captures[1]
-			end), t([[}{]]), i(1), t([[}]]) },
-			context.math()
-		),
-		s(
-			{ trig = [=[^(.*[%)%}])/]=], wordTrig = false, regTrig = true },
-			{
-				f(function(_, snip)
-					local str = snip.captures[1]
-					local idx = #str
-          print(idx)
-					local depth = 0
+    -- Fractions
+    s(
+      { trig = "//", wordTrig = false },
+      { t([[\frac{]]), i(1), t([[}{]]), i(2), t([[}]]) },
+      context.math()
+    ),
+    s(
+      { trig = [=[([a-zA-Z0-9_^\{}]+)/]=], wordTrig = false, regTrig = true },
+      {
+        t([[\frac{]]),
+        f(function(_, snip)
+          return snip.captures[1]
+        end),
+        t([[}{]]),
+        i(1),
+        t([[}]]),
+      },
+      context.math()
+    ),
+    s({ trig = [=[^(.*[%)%}])/]=], wordTrig = false, regTrig = true }, {
+      f(function(_, snip)
+        local str = snip.captures[1]
+        local idx = #str
+        print(idx)
+        local depth = 0
 
-					while idx > 0 do
-						local char = str:sub(idx, idx)
-            print(char)
-						if char == ")" or char == "}" then
-							depth = depth - 1
-						end
-						if char == "(" or char == "(" then
-							depth = depth + 1
-						end
+        while idx > 0 do
+          local char = str:sub(idx, idx)
+          print(char)
+          if char == ")" or char == "}" then
+            depth = depth - 1
+          end
+          if char == "(" or char == "(" then
+            depth = depth + 1
+          end
 
-            print(depth)
-						if depth == 0 then
-							break
-						end
-						idx = idx - 1
-					end
+          print(depth)
+          if depth == 0 then
+            break
+          end
+          idx = idx - 1
+        end
 
-          return str:sub(0, idx - 1) .. [[\frac{]] .. str:sub(idx)
-				end),
-				t([[}{]]),
-				i(1),
-				t([[}]]),
-			},
-			context.math()
-		),
+        return str:sub(0, idx - 1) .. [[\frac{]] .. str:sub(idx)
+      end),
+      t([[}{]]),
+      i(1),
+      t([[}]]),
+    }, context.math()),
 
-		-- Align environment
-		s("ali", { t({ [[\begin{align}]], "  " }), i(1), t({ "", [[\end{align}]] }) }, context.notmath()),
+    -- Align environment
+    s(
+      "ali",
+      { t({ [[\begin{align}]], "  " }), i(1), t({ "", [[\end{align}]] }) },
+      context.notmath()
+    ),
 
-		s("mlt", { t({ [[\begin{multline*}]], "  " }), i(1), t({ "", [[\end{multline*}]] }) }, context.notmath()),
-	}
+    s(
+      "mlt",
+      { t({ [[\begin{multline*}]], "  " }), i(1), t({ "", [[\end{multline*}]] }) },
+      context.notmath()
+    ),
+  }
