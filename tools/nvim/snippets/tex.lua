ef95c46fb357167d19c3a117e24f3aa393813573
Aiko Wessels <aiko.wessels@gmail.com>
2022-06-14 08:35:19 +0200

    feat(nvim): add latex snippets

--- tools/nvim/snippets/tex.lua
+++ tools/nvim/snippets/tex.lua
@@ -1,10 +1,19 @@
 local ls = require("luasnip")
 local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
 local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
 local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
 local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local m = require("luasnip.extras").m
+local lambda = require("luasnip.extras").l
 
 -- ----------------------
 -- |   Vimtex Regions   |
@@ -37,6 +46,8 @@ local ctx = {
 -- |   Helper Functions   |
 -- ------------------------
 
+-- Register an auto-command to insert a space after a snippet if the following
+-- character is a letter.
 local autoinsert_space = {
   -- index `-1` means the callback is on the snippet as a whole
   [-1] = {
@@ -54,134 +65,303 @@ local autoinsert_space = {
   },
 }
 
--- ----------------
--- |   Snippets   |
--- ----------------
+-- Convert content of node at index to snake case.
+local to_snake = function(index)
+  return f(function(args)
+    s = args[1][1]
+    s = s:gsub("ü", "ue")
+    s = s:gsub("Ü", "Ue")
+    s = s:gsub("ä", "ae")
+    s = s:gsub("Ä", "Ae")
+    s = s:gsub("ö", "oe")
+    s = s:gsub("Ö", "Oe")
+    s = s:gsub("ß", "ss")
 
-return {},
+    s = s:lower(s)
+    return s:gsub("%W+", "_")
+  end, { index })
+end
+
+-- Show text if node at index is not empty
+local show_not_empty = function(index, text)
+  return f(function(args)
+    if #args[1] == 0 then return "" end
+    if args[1][1]:len() == 0 then return "" end
+    return text
+  end, { index })
+end
+
+-- Create a choice node which simply toggles text
+local toggle_text = function(index, text, reverse)
+  if reverse then
+    return c(index, {
+      t(text),
+      t(""),
+    })
+  else
+    return c(index, {
+      t(""),
+      t(text),
+    })
+  end
+end
+
+-- Recursive list using snippet node
+local recursive_list
+recursive_list = function()
+  return sn(nil, {
+    c(1, {
+      -- important!! Having the sn(...) as the first choice will cause infinite recursion.
+      t({ "" }),
+      -- The same dynamicNode as in the snippet (also note: self reference).
+      sn(nil, { t({ "", "\t\\item " }), i(1), d(2, recursive_list, {}) }),
+    }),
+  })
+end
+
+-- Dynamically create a bunch of snippet nodes parsed from a latex table string.
+local table_node = function(args)
+  local tabs = {}
+  local table = args[1][1]:gsub("%s", ""):gsub("|", "")
+  local count = table:len()
+
+  for j = 1, count do
+    local i_node = i(j)
+    tabs[2 * j - 1] = i_node
+    if j ~= count then
+      tabs[2 * j] = t " & "
+    end
+  end
+
+  return sn(nil, tabs)
+end
+
+local rec_table
+rec_table = function()
+  return sn(nil, {
+    c(1, {
+      t({ "" }),
+      sn(nil, {
+        t({ [[\\]], [[]] }),
+        d(1, table_node, { ai[1] }),
+        d(2, rec_table, { ai[1] }),
+      })
+    })
+  })
+end
+
+-- =========================
+-- |=======================|
+-- ||                     ||
+-- ||   Manual Snippets   ||
+-- ||                     ||
+-- |=======================|
+-- =========================
+return {
+  s(
+    { trig = "^%s*table", regTrig = true },
+    fmta([[
+      \begin{table}[<position>]
+        \centering
+        \caption{<caption>}
+        \label{tab:<label>}
+        \begin{tabular}{<columns>}
+          <tn><rec>
+        \end{tabular}
+      \end{table}
+    ]], {
+      position = i(1, "htbp"),
+      caption = i(2, "caption"),
+      label = i(3, "label"),
+      columns = i(4, "c c c"),
+      tn = d(5, table_node, { 1 }, {}),
+      rec = d(6, rec_table, { 1 }),
+    })
+  ),
+  s(
+    { trig = "^%s*fig", regTrig = true },
+    fmta(
+      [[
+        \begin{figure}[<position>]
+          \centering
+          <include>
+          \caption{<caption>}
+          \label{fig:<label>}
+        \end{figure}
+      ]], {
+      position = i(1, "htbp"),
+      include = c(2, {
+        sn(nil, {
+          t([[\includegraphics[width=0.8\textwidth]{]]),
+          i(1),
+          t([[}]])
+        }),
+        t(""),
+      }),
+      caption = i(2, "caption"),
+      label = i(3, "label"),
+    })
+  )
+}, {
+  -- ======================
+  -- |====================|
+  -- ||                  ||
+  -- ||   Autosnippets   ||
+  -- ||                  ||
+  -- |====================|
+  -- ======================
+  -- --------------------
+  -- |   Environments   |
+  -- --------------------
+  s(
+    { trig = "^%s*beg", regTrig = true },
+    fmta(
+      [[
+        \begin{<1>}<3><2><4>
+          <0>
+        \end{<1>}
+      ]], {
+      [1] = i(1),
+      [2] = i(2),
+      [3] = show_not_empty(2, "{"),
+      [4] = show_not_empty(2, "}")
+    })
+  ),
+
+  -- -----------------------
+  -- |   Enter Math mode   |
+  -- -----------------------
+  -- Inline math
+  s(
+    { trig = "mk", priority = 100 },
+    fmta([[$<1>$]], { i(1) }),
     {
-      -- -----------------------
-      -- |   Enter Math mode   |
-      -- -----------------------
-      -- Inline math
-      s(
-        { trig = "mk", priority = 100 },
-        fmt([[${1}$]], { i(1) }),
-        {
-          condition = ctx.math,
-          show_condition = ctx.math,
-          callbacks = autoinsert_space
-        }
-      ),
-
-      s(
-        { trig = "([%w_]+)mk", regTrig = true, priority = 200 },
-        f(function(_, snip)
-          return "$" .. snip.captures[1] .. "$"
-        end),
-        {
-          condition = ctx.not_math,
-          show_condition = ctx.not_math,
-          callbacks = autoinsert_space,
-        }
-      ),
-
-      -- Display math
-      s(
-        "dm",
-        fmt(
-          [[
-          \[
-            {1}
-          \]
-        ]] ,
-          i(1)
-        ),
-        {
-          condition = ctx.not_math,
-          show_condition = ctx.not_math,
-        }
-      ),
-
-      -- -----------------
-      -- |   Fractions   |
-      -- -----------------
-      s(
-        { trig = "//", wordTrig = false },
-        { t([[\frac{]]), i(1), t([[}{]]), i(2), t([[}]]) },
-        {
-          condition = ctx.math,
-          show_condition = ctx.math,
-        }
-      ),
-      s({ trig = [=[([a-zA-Z0-9_^\{}]+)/]=], wordTrig = false, regTrig = true }, {
-        t([[\frac{]]),
-        f(function(_, snip)
-          return snip.captures[1]
-        end),
-        t([[}{]]),
-        i(1),
-        t([[}]]),
-      },
-        {
-          condition = ctx.math,
-          show_condition = ctx.math,
-        }
-      ),
-      s({ trig = [=[^(.*[%)%}])/]=], wordTrig = false, regTrig = true }, {
-        f(function(_, snip)
-          local str = snip.captures[1]
-          local idx = #str
-          print(idx)
-          local depth = 0
-
-          while idx > 0 do
-            local char = str:sub(idx, idx)
-            print(char)
-            if char == ")" or char == "}" then
-              depth = depth - 1
-            end
-            if char == "(" or char == "(" then
-              depth = depth + 1
-            end
-
-            print(depth)
-            if depth == 0 then
-              break
-            end
-            idx = idx - 1
-          end
-
-          return str:sub(0, idx - 1) .. [[\frac{]] .. str:sub(idx)
-        end),
-        t([[}{]]),
-        i(1),
-        t([[}]]),
-      },
-        {
-          condition = ctx.math,
-          show_condition = ctx.math,
-        }
-      ),
-
-      -- -------------------------
-      -- |   Math Environments   |
-      -- -------------------------
-      s(
-        "ali",
-        { t({ [[\begin{align}]], "  " }), i(1), t({ "", [[\end{align}]] }) },
-        {
-          condition = ctx.not_math,
-          show_condition = ctx.not_math,
-        }
-      ),
-
-      s(
-        "mlt",
-        { t({ [[\begin{multline*}]], "  " }), i(1), t({ "", [[\end{multline*}]] }) },
-        {
-          condition = ctx.not_math,
-          show_condition = ctx.not_math,
-        }
-      ),
+      condition = ctx.math,
+      show_condition = ctx.math,
+      callbacks = autoinsert_space
     }
+  ),
+
+  -- Surround letter with inline math
+  s(
+    { trig = "([%w])mk", regTrig = true, priority = 200 },
+    f(function(_, snip)
+      return "$" .. snip.captures[1] .. "$"
+    end),
+    {
+      condition = ctx.not_math,
+      show_condition = ctx.not_math,
+      callbacks = autoinsert_space,
+    }
+  ),
+
+  -- Display math
+  s(
+    "dm",
+    fmta(
+      [[
+        \[
+          <1>
+        \]
+      ]], {
+      i(1)
+    }),
+    {
+      condition = ctx.not_math,
+      show_condition = ctx.not_math,
+    }
+  ),
+
+  -- -----------------
+  -- |   Fractions   |
+  -- -----------------
+  s(
+    { trig = "//", wordTrig = false },
+    fmta([[\frac{<1>}{<2>}]], { i(1), i(2) }),
+    {
+      condition = ctx.math,
+      show_condition = ctx.math,
+    }
+  ),
+  s({ trig = [=[([a-zA-Z0-9_^\{}]+)/]=], wordTrig = false, regTrig = true }, {
+    fmta([[\frac{<1>}{<2>}]], {
+      f(function(_, snip)
+        return snip.captures[1]
+      end),
+      i(1),
+    })
+  },
+    {
+      condition = ctx.math,
+      show_condition = ctx.math,
+    }
+  ),
+  s({ trig = [=[^(.*[%)%}])/]=], regTrig = true }, {
+    f(function(_, snip)
+      local str = snip.captures[1]
+      local idx = #str
+      print(idx)
+      local depth = 0
+
+      while idx > 0 do
+        local char = str:sub(idx, idx)
+        print(char)
+        if char == ")" or char == "}" then
+          depth = depth - 1
+        end
+        if char == "(" or char == "(" then
+          depth = depth + 1
+        end
+
+        print(depth)
+        if depth == 0 then
+          break
+        end
+        idx = idx - 1
+      end
+
+      return str:sub(0, idx - 1) .. [[\frac{]] .. str:sub(idx)
+    end),
+    t([[}{]]),
+    i(1),
+    t([[}]]),
+  },
+    {
+      condition = ctx.math,
+      show_condition = ctx.math,
+    }
+  ),
+
+  -- -------------------------
+  -- |   Math Environments   |
+  -- -------------------------
+  s(
+    { trig = "^%s*ali", regTrig = true },
+    fmta(
+      [[
+        \begin{align<1>}
+          <2>
+        \end{align<1>}
+      ]],
+      { i(1), toggle_text(2, "*") })
+    {
+      condition = ctx.not_math,
+      show_condition = ctx.not_math,
+    }
+  ),
+
+  s(
+    { trig = "^%s*mlt", regTrig = true },
+    fmta(
+      [[
+        \begin{multline<1>}
+          <2>
+        \end{multline<1>}
+      ]],
+      { i(1), toggle_text(2, "*") })
+    {
+      condition = ctx.not_math,
+      show_condition = ctx.not_math,
+    }
+  ),
+}
