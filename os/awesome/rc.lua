b963acbbf4ee2823ad6b9d564ed6285303e19607
Aiko Wessels <aiko.wessels@gmail.com>
2022-06-26 16:00:53 +0200

    fix(awesome): fix duplicated mappings for movement

--- os/awesome/rc.lua
+++ os/awesome/rc.lua
@@ -279,14 +279,6 @@ local globalkeys = my_table.join(
     { description = "show help", group = "awesome" }
   ),
 
-  awful.key({ modkey }, "j", function()
-    awful.client.focus.byidx(1)
-  end, { description = "focus next by index", group = "client" }),
-
-  awful.key({ modkey }, "k", function()
-    awful.client.focus.byidx(-1)
-  end, { description = "focus previous by index", group = "client" }),
-
   awful.key({ modkey }, "w", function()
     mymainmenu:show()
   end, { description = "show main menu", group = "awesome" }),
