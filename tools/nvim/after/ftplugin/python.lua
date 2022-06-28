3101ddf15ac7fd5297b93f6530084206d659d307
Aiko Wessels <aiko.wessels@gmail.com>
2022-06-28 18:16:31 +0200

    feat(nvim): add python compiler and makeprg

--- tools/nvim/after/ftplugin/python.lua
+++ tools/nvim/after/ftplugin/python.lua
@@ -7,18 +7,33 @@ vim.opt_local.softtabstop = 4
 vim.opt_local.fileformat = "unix"
 vim.opt_local.textwidth = 80
 
+-- Set the indent after opening parenthesis
+vim.g.pyindent_open_paren = vim.bo.shiftwidth
+vim.g.pyindent_nested_paren = vim.bo.shiftwidth
+vim.g.pyindent_continue = vim.bo.shiftwidth
+
+if vim.fn.executable("black") == 1 then
+  vim.opt_local.formatprg = [[black --quiet - 2>/dev/null]]
+end
+
+vim.cmd([[compiler flake8]])
+
+if vim.fn.executable("python3") == 1 then
+  vim.keymap.set("n", "<localleader>r", [[:!python3 %<CR>]])
+end
+
 -- Create a buffer local keymap to reformat, using the buffer local
 -- command.
 vim.keymap.set(
   "n",
-  "<localleader>rf",
+  "<localleader>f",
   require("python-nvim").format,
   { silent = true, buffer = 0, desc = "reformat python with black and isort" }
 )
 
 vim.keymap.set(
   "n",
-  "<localleader>if",
+  "<localleader>i",
   require("python-nvim").flake8,
   { buffer = 0, desc = "run flake8 linting" }
 )
