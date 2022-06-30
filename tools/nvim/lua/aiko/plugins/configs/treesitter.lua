64948be0eef14ba9c0bad34bb3b1f62683d8142f
Aiko Wessels <aiko.wessels@gmail.com>
2022-06-30 08:55:05 +0200

    fix: remove broken markdown treesitter parser

--- tools/nvim/lua/aiko/plugins/configs/treesitter.lua
+++ tools/nvim/lua/aiko/plugins/configs/treesitter.lua
@@ -16,7 +16,7 @@ M.setup = function()
       "go",
       "json",
       "lua",
-      "markdown",
+      -- "markdown", -- Problems with markdown parser currently
       "python",
       "rust",
       "toml",
