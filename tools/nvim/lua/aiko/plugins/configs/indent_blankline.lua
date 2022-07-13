3e044ec351513c72a2a73a9c76c2055b6206ce01
Aiko Wessels <aiko.wessels@gmail.com>
2022-07-13 18:18:48 +0200

    fix(nvim): fix bugs caused by refactoring

--- tools/nvim/lua/aiko/plugins/configs/indent_blankline.lua
+++ tools/nvim/lua/aiko/plugins/configs/indent_blankline.lua
@@ -22,7 +22,7 @@ M.setup = function()
     show_trailing_blankline_indent = false,
     show_first_indent_level = false,
     show_current_context = true,
-    show_current_context_start = true,
+    show_current_context_start = false,
   })
 end
 
