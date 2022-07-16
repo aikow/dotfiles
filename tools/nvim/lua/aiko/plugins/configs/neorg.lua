7702dc354ba71e2b7c791b18f1a17364a73ed3df
Aiko Wessels <aiko.wessels@gmail.com>
2022-07-16 14:28:55 +0200

    feat(nvim): setup neorg for norg mode

--- tools/nvim/lua/aiko/plugins/configs/neorg.lua
+++ tools/nvim/lua/aiko/plugins/configs/neorg.lua
@@ -8,9 +8,32 @@ M.setup = function()
 
   neorg.setup({
     load = {
+      -- Builtin modules
       ["core.defaults"] = {},
+      ["core.norg.dirman"] = {
+        config = {
+          workspaces = {
+            personal = "~/GDrive/notes",
+          },
+        },
+      },
       ["core.norg.concealer"] = {},
-      ["core.norg.completion"] = {},
+      ["core.norg.completion"] = {
+        config = {
+          engine = "nvim-cmp",
+        },
+      },
+      ["core.presenter"] = {},
+      ["core.norg.journal"] = {
+        config = {
+          workspace = "personal",
+          journal_folder = "journal",
+          strategy = "nested",
+        },
+      },
+
+      -- External Modules
+      ["core.integrations.telescope"] = {},
     },
   })
 end
