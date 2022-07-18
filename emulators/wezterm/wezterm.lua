6dd5cf5ce3581bcf734ae7aa98c62c185f638be4
Aiko Wessels <aiko.wessels@gmail.com>
2022-07-18 09:55:32 +0200

    feat(wezterm): add config and colorscheme

---
+++ emulators/wezterm/wezterm.lua
@@ -0,0 +1,33 @@
+local wezterm = require("wezterm")
+
+-- Spawn a fish shell in login mode
+local shell = { "/usr/bin/fish", "-l" }
+
+local catppuccin = require("colors/catppuccin").setup({
+  -- whether or not to sync with the system's theme
+  sync = true,
+  -- the flavours to switch between when syncing
+  -- available flavours: "latte" | "frappe" | "macchiato" | "mocha"
+  sync_flavours = {
+    light = "latte",
+    dark = "macchiato",
+  },
+  -- the default/fallback flavour, when syncing is disabled
+  flavour = "macchiato",
+})
+
+return {
+  default_prog = shell,
+
+  colors = catppuccin,
+
+  enable_tab_bar = false,
+
+  -- font = wezterm.font("Hack Nerd Font"),
+  font = wezterm.font_with_fallback({
+    "Hack Nerd Font",
+    "JetBrains Mono",
+  }),
+
+  font_size = 11,
+}
