48654be763ef5691ed94bc1397719dac50596eb9
Aiko Wessels <aiko.wessels@gmail.com>
2022-08-01 10:07:12 +0200

    fix: update options for macos

--- tools/nvim/lua/aiko/config/neovide.lua
+++ tools/nvim/lua/aiko/config/neovide.lua
@@ -1,1 +1,6 @@
-vim.opt.guifont = "FiraCode Nerd Font:h11"
+if vim.fn.has("macunix") == 1 then
+  vim.opt.guifont = "FiraCode Nerd Font:h13"
+  vim.g.neovide_input_macos_alt_is_meta = true
+else
+  vim.opt.guifont = "FiraCode Nerd Font:h11"
+end
