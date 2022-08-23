9f8f9f6357a1509c0f7b3be0fc8c6419cb3f75a8
Aiko Wessels <aiko.wessels@gmail.com>
2022-08-23 13:28:09 +0200

    fix(wezterm): fish path on macos

--- emulators/wezterm/wezterm.lua
+++ emulators/wezterm/wezterm.lua
@@ -1,7 +1,30 @@
 local wezterm = require("wezterm")
 
+local capture = function(cmd, raw)
+  local f = assert(io.popen(cmd, "r"))
+  local s = assert(f:read("*a"))
+  f:close()
+
+  if raw then
+    return s
+  end
+
+  -- Trim each line and normalize new line characters.
+  s = string.gsub(s, "^%s+", "")
+  s = string.gsub(s, "%s+$", "")
+  s = string.gsub(s, "[\n\r]+", "\n")
+  return s
+end
+
+local hostname = capture("uname")
+
 -- Spawn a fish shell in login mode
-local shell = { "/usr/bin/fish", "-l" }
+local shell
+if hostname == "Darwin" then
+  shell = { "/usr/local/bin/fish", "-l" }
+else
+  shell = { "/usr/bin/fish", "-l" }
+end
 
 local catppuccin = require("colors/catppuccin").setup({
   -- whether or not to sync with the system's theme
