8660e026b7246a6b613c9ee99797495944bd2273
Aiko Wessels <aiko.wessels@gmail.com>
2022-10-07 13:49:07 +0200

    fix(fish): fzf fish variables script missing parameters

--- tools/nvim/after/ftplugin/python.lua
+++ tools/nvim/after/ftplugin/python.lua
@@ -4,14 +4,11 @@ vim.opt_local.smarttab = true
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
-vim.opt_local.fileformat = "unix"
+-- vim.opt_local.fileformat = "unix"
 vim.opt_local.textwidth = 80
 
 -- Set the indent after opening parenthesis
 vim.g.pyindent_open_paren = vim.bo.shiftwidth
--- vim.g.pyindent_nested_paren = vim.bo.shiftwidth
--- vim.g.pyindent_continue = vim.bo.shiftwidth
 
-if vim.fn.executable("python3") == 1 then
-  vim.keymap.set({ "n", "v" }, "<localleader>r", [[:!python3 %<CR>]])
-end
+-- Shortcut to run file through interpreter.
+vim.keymap.set({ "n", "v" }, "<localleader>r", [[:!python3 %<CR>]])
