52f431c24d97b1ab193fb16a462f7136db18f8a2
Aiko Wessels <aiko.wessels@gmail.com>
2022-11-03 18:37:31 +0100

    feat(nvim): add custom completions to python sql format

--- tools/nvim/after/ftplugin/python.lua
+++ tools/nvim/after/ftplugin/python.lua
@@ -125,7 +125,7 @@ vim.api.nvim_buf_create_user_command(0, "SqlFormat", function(opts)
     [[;; query
       (assignment
         left: (identifier) @_id (#contains? @_id "query")
-        right: (string) @sql (#offset! @sql 1 0 -1 0))
+        right: (string) @sql)
     ]],
     function(text)
       -- FIXME: Indentation is at the first of the triple quote ("""). Ideally
@@ -134,21 +134,29 @@ vim.api.nvim_buf_create_user_command(0, "SqlFormat", function(opts)
       -- Clean the input text.
       text = string.gsub(string.sub(text, 4, -4), "\n", "")
 
+      -- Default cleaning rules for identifiers.
+      local clean = setmetatable({
+        sqlite = {
+          pre = { pat = "%?", rep = "__id__" },
+          post = { pat = "__id__", rep = "%?" },
+        },
+      }, {
+        -- Set the default cleaning rules.
+        __index = {
+          pre = { pat = "%${(%d)}", rep = "__id_%1__" },
+          post = { pat = "__id_(%d)__", rep = "%${%1}" },
+        },
+      })
+
+      local subs = clean[lang]
+
       -- Perform replacements of the identifiers to make the text valid SQL.
-      if lang == "sqlite" then
-        text = string.gsub(text, "%?", "__id__")
-      else
-        text = string.gsub(text, "${(%d)}", "__id_%1__")
-      end
+      text = string.gsub(text, subs.pre.pat, subs.pre.rep)
 
       text = vim.fn.system("sql-formatter -l " .. lang, text)
 
       -- Revert the previous substitutions.
-      if lang == "sqlite" then
-        text = string.gsub(text, "__id__", "%?")
-      else
-        text = string.gsub(text, "__id_(%d)__", "${%1}")
-      end
+      text = string.gsub(text, subs.post.pat, subs.post.rep)
 
       -- Split the text on newlines.
       local lines = vim.split(text, "\n")
@@ -158,9 +166,31 @@ vim.api.nvim_buf_create_user_command(0, "SqlFormat", function(opts)
 
       return lines
     end,
-    { filetype = "python", capture = "sql" }
+    {
+      filetype = "python",
+      capture = "sql",
+    }
   )
 end, {
   desc = "Automatically format SQL statements in python files",
   nargs = "*",
+  complete = function()
+    return {
+      "bigquery",
+      "db2",
+      "hive",
+      "mariadb",
+      "mysql",
+      "n1ql",
+      "plsql",
+      "postgresql",
+      "redshift",
+      "singlestoredb",
+      "spark",
+      "sql",
+      "sqlite",
+      "trino",
+      "tsql",
+    }
+  end,
 })
