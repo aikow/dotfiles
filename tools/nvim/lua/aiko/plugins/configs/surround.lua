2659e2f865dd241f6726a17fa50d3fcafa28da4f
Aiko Wessels <aiko.wessels@gmail.com>
2022-11-03 18:36:45 +0100

    feat(nvim): configure nvim-surround and default maps

--- tools/nvim/lua/aiko/plugins/configs/surround.lua
+++ tools/nvim/lua/aiko/plugins/configs/surround.lua
@@ -1,7 +1,29 @@
 local M = {}
 
 M.setup = function()
-  require("nvim-surround").setup({})
+  require("nvim-surround").setup({
+    keymaps = {
+      insert = "<C-g>s",
+      insert_line = "<C-g>S",
+      normal = "gs",
+      normal_cur = "gss",
+      normal_line = "gS",
+      normal_cur_line = "gSS",
+      visual = "S",
+      visual_line = "gS",
+      delete = "ds",
+      change = "cs",
+    },
+    alises = {
+      ["a"] = ">",
+      ["b"] = ")",
+      ["B"] = "}",
+      ["r"] = "]",
+      ["q"] = { '"', "'", "`" },
+      ["s"] = { "}", "]", ")", ">", '"', "'", "`" },
+    },
+    move_cursor = true,
+  })
 end
 
 return M
