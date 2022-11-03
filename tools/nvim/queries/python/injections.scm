9ed30f583e96faf1cdd3b823cc259d211653d910
Aiko Wessels <aiko.wessels@gmail.com>
2022-11-03 18:35:03 +0100

    feat(nvim): update tree-sitter python injections

--- tools/nvim/queries/python/injections.scm
+++ tools/nvim/queries/python/injections.scm
@@ -1,3 +1,7 @@
 (assignment
   left: (identifier) @_id (#contains? @_id "query")
-  right: (string) @sql (#offset! @sql 1 0 0 0))
+  right: (string) @sql)
+
+(assignment
+  left: (identifier) @_id
+  right: (string) @sql (#lua-match? @sql "^\"+%s*-- [Qq]uery"))
