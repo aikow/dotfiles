becfb6b8143cfab9c7e98194ced8ae01f97ae1bb
Aiko Wessels <aiko.wessels@gmail.com>
2022-12-05 08:26:50 +0100

    feat(nvim): add regex injections for rust

--- config/nvim/after/queries/rust/injections.scm
+++ config/nvim/after/queries/rust/injections.scm
@@ -7,3 +7,12 @@
     field: (field_identifier) @_fn (#vim-match? @_fn "^(execute|query|prepare)"))
   arguments: (arguments
     (raw_string_literal) @sql))
+
+;; Regex
+;; Regex
+; (call_expression
+;   function: (scoped_identifier
+;     path: (identifier) @_mod (#any-of? @_mod "Regex")
+;     name: (identifier) @_fn (#any-of? @_fn "new"))
+;   arguments: (arguments
+;     [(raw_string_literal) (concatenated_string)] @regex))
