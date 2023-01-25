4e84d691778fa0dee9976d35c5e5323888d127a8
Aiko Wessels <aiko.wessels@gmail.com>
2023-01-25 17:18:47 +0100

    feat(sh): remove levels from lt

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -3,8 +3,8 @@ if command -v exa &>/dev/null
   alias l='exa'
   alias ls='exa --group-directories-first --icons'
   alias lg='exa -l --group-directories-first --icons --git'
+  alias lt='exa --group-directories-first --tree --icons'
   alias ll='exa -l --group-directories-first --icons'
-  alias lt='exa --group-directories-first --tree --level 2 --icons'
   alias lll='exa -la --group-directories-first --icons --group'
 else
   alias l='ls'
