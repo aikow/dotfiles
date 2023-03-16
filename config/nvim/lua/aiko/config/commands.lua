3822364c13447feaceeab68606575751f9d5d790
Aiko Wessels <aiko.wessels@gmail.com>
2023-03-16 13:08:49 +0100

    feat(nvim): add command to insert random string

--- config/nvim/lua/aiko/config/commands.lua
+++ config/nvim/lua/aiko/config/commands.lua
@@ -17,24 +17,24 @@ end, {
   desc = "Print the syntax group and highlight group of the token under the cursor",
 })
 
-vim.api.nvim_create_user_command("ReloadConfig", function()
-  if vim.bo.buftype == "" then
-    if vim.fn.exists(":LspStop") ~= 0 then
-      vim.cmd.LspStop()
-    end
+vim.api.nvim_create_user_command("Random", function(opts)
+  local len = opts.args ~= "" and opts.args or 8
+  print(len)
+  local chars = "abcdefghijklmnopqrstuvwxyz0123456789"
 
-    for name, _ in pairs(package.loaded) do
-      if name:match("^user") then
-        package.loaded[name] = nil
-      end
-    end
-
-    dofile(vim.env.MYVIMRC)
-    vim.cmd.PackerCompile()
-    vim.notify("Compiling with packer", vim.log.levels.DEBUG)
-  else
-    vim.notify("Not available in this window/buffer", vim.log.levels.INFO)
+  -- 65 for uppercase
+  -- 97 for lowercase
+  local rand = ""
+  for _ = 1, len do
+    local idx = math.random(1, #chars)
+    rand = rand .. string.sub(chars, idx, idx)
   end
+
+  local pos = vim.api.nvim_win_get_cursor(0)[2]
+  local line = vim.api.nvim_get_current_line()
+  local nline = line:sub(0, pos) .. rand .. line:sub(pos + 1)
+  vim.api.nvim_set_current_line(nline)
 end, {
   desc = "Reload and recompile the entire neovim configuration.",
+  nargs = "?",
 })
