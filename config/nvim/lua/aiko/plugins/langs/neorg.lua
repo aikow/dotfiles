b61cfd1e0a6e236b38e0a9e28bdb94f4bf3dc36c
Aiko Wessels <aiko.wessels@gmail.com>
2023-03-28 16:59:37 +0200

    feat(nvim): add more neorg mappings

--- config/nvim/lua/aiko/plugins/langs/neorg.lua
+++ config/nvim/lua/aiko/plugins/langs/neorg.lua
@@ -15,6 +15,14 @@ M.setup = function()
             keybinds.map("norg", "n", "<localleader>mu", "<cmd>Neorg update-metadata<CR>")
             keybinds.map("norg", "n", "<localleader>t", "<cmd>Neorg tangle current-file<CR>")
 
+            -- Telescope
+            keybinds.map("norg", "i", "<C-l>", "<cmd>Telescope neorg insert_link<CR>")
+            keybinds.map("norg", "i", "<C-h>", "<cmd>Telescope neorg insert_file_link<CR>")
+
+            keybinds.map("norg", "n", "<localleader>fo", "<cmd>Telescope neorg find_norg_files<CR>")
+            keybinds.map("norg", "n", "<localleader>fl", "<cmd>Telescope neorg find_linkable<CR>")
+            keybinds.map("norg", "n", "<localleader>fh", "<cmd>Telescope neorg find_headings<CR>")
+
             -- Looking glass
             keybinds.map_event("norg", "n", "<localleader>c", "core.looking-glass.magnify-code-block")
           end,
