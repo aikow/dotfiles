d121cc3018262581c8142d8ae03b4c4b812e3b84
Aiko Wessels <aiko.wessels@gmail.com>
2023-03-31 16:00:12 +0200

    fix(wezterm): rename deprecated config keys

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,54 +1,31 @@
 local wezterm = require("wezterm")
+local util = require("util")
 
----Capture command output as a single string with newlines.
----@param cmd string The command to run
----@param trim boolean If set to true, strip leading and trailing whitespace
----from each line
----@param normalize_line_endings boolean If set to true, replace windows line
----endings with linux ones.
----@return string
-local capture = function(cmd, opts)
-  opts = opts or {}
-  local f = assert(io.popen(cmd, "r"))
-  local s = assert(f:read("*a"))
-  f:close()
+local config = {}
 
-  -- Trim each line and normalize new line characters.
-  if opts.trim or true then
-    s = string.gsub(s, "^%s+", "")
-    s = string.gsub(s, "%s+$", "")
-  end
+-- Colors
+config.color_scheme = "Gruvbox dark, medium (base16)"
 
-  if opts.normalize_line_endings or true then
-    s = string.gsub(s, "[\n\r]+", "\n")
-  end
-  return s
+-- Appearance
+config.default_cursor_style = "SteadyBar"
+config.enable_tab_bar = true
+
+-- Fonts
+config.font = wezterm.font_with_fallback({ "Hack Nerd Font", "JetBrains Mono" })
+config.font_size = 11
+config.use_ime = false
+
+-- Behavior
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = false
+
+-- Shell
+config.default_prog = { "/usr/bin/fish", "-l" }
+
+-- MacOS specific overrides.
+if util.capture("uname") == "Darwin" then
+  config.default_prog = { "/usr/local/bin/fish", "-l" }
+  config.font_size = 13
 end
 
-local hostname = capture("uname")
-
--- Spawn a fish shell in login mode
-local shell
-local font_size
-if hostname == "Darwin" then
-  shell = { "/usr/local/bin/fish", "-l" }
-  font_size = 13
-else
-  shell = { "/usr/bin/fish", "-l" }
-  font_size = 11
-end
-
-return {
-  default_prog = shell,
-  color_scheme = "Gruvbox Dark",
-  default_cursor_style = "SteadyBar",
-  enable_tab_bar = false,
-  send_composed_key_when_left_alt_is_pressed = false,
-  send_composed_key_when_right_alt_is_pressed = false,
-  use_ime = false,
-  font = wezterm.font_with_fallback({
-    "Hack Nerd Font",
-    "JetBrains Mono",
-  }),
-  font_size = font_size,
-}
+return config
