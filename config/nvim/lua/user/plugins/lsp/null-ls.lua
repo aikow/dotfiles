e61bb621e13a5f1813e10e12d8ca7b072fa8c1a7
Aiko Wessels <aiko.wessels@gmail.com>
2023-07-03 14:53:53 +0200

    fix(nvim): cleanup null-ls setup with mason

--- config/nvim/lua/user/plugins/lsp/null-ls.lua
+++ config/nvim/lua/user/plugins/lsp/null-ls.lua
@@ -5,12 +5,18 @@ M.opts = {
   black = {
     with = { extra_args = { "--preview" } },
   },
+  fish_indent = {
+    null = true,
+    methods = { "formatting" },
+  },
+  fish = {
+    null = true,
+    methods = { "diagnostics" },
+  },
   isort = {
     with = { extra_args = { "--profile=black" } },
   },
-  jq = {
-    enabled = false,
-  },
+  jq = {},
   ruff = {
     methods = { "diagnostics" },
   },
@@ -28,27 +34,52 @@ M.opts = {
       end,
     },
   },
+  stylua = {
+    null = true,
+    methods = { "formatting" },
+  },
 }
 
 M.setup = function(_, opts)
   local null_ls = require("null-ls")
   local mason_null = require("mason-null-ls")
 
-  local nls = require("null-ls")
-  null_ls.setup({
-    sources = {
-      nls.builtins.formatting.fish_indent,
-      nls.builtins.diagnostics.fish,
-    },
-  })
+  local mason_sources = {}
+  local null_sources = {}
+
+  -- Partition the sources into sources managed by mason and sources managed
+  -- directly by null-ls. The partitioning is done by checking the `null`
+  -- field of the source's options.
+  for source_name, source_opts in pairs(opts) do
+    source_opts = source_opts or {}
+    -- If `null` isn't a table field, default to false.
+    if source_opts.null then
+      for _, method in pairs(source_opts.methods or {}) do
+        table.insert(
+          null_sources,
+          null_ls.builtins[method][source_name].with(source_opts.with or {})
+        )
+      end
+    else
+      mason_sources[source_name] = source_opts
+    end
+  end
 
   mason_null.setup({
     ensure_installed = { "jq" },
     automatic_installation = false,
     automatic_setup = true,
     handlers = {
+      -- Use the generic handler function to inject extra arguments for the
+      -- sources.
       function(source_name, methods)
-        local source_opts = opts[source_name] or {}
+        local source_opts = mason_sources[source_name] or {}
+
+        -- Set a default value for `enabled`.
+        if source_opts.enabled == nil then
+          source_opts.enabled = true
+        end
+
         if source_opts.enabled then
           for _, method in pairs(source_opts.methods or methods) do
             null_ls.register(
@@ -58,6 +89,11 @@ M.setup = function(_, opts)
         end
       end,
     },
+  })
+
+  -- Setup all sources not managed by mason-null-ls.
+  null_ls.setup({
+    sources = null_sources,
   })
 end
 
