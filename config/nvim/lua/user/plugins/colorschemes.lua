765a028a60ae2297f35b02f7c57821fedea391e7
Aiko Wessels <aiko.wessels@gmail.com>
2023-07-04 16:50:27 +0200

    chore(nvim): move colorscheme logic into a separate repo

---
+++ config/nvim/lua/user/plugins/colorschemes.lua
@@ -0,0 +1,6 @@
+return {
+  { "aikow/base.nvim" },
+  { "ribru17/bamboo.nvim", lazy = true },
+  { "catppuccin/nvim", name = "catppuccin.nvim", lazy = true },
+  { "folke/tokyonight.nvim", lazy = true },
+}
