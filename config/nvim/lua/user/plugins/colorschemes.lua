e419b1a6b2a74b2d462d22f0a5b984fbd8d787fb
Aiko Wessels <aiko.wessels@gmail.com>
2023-07-05 07:15:24 +0200

    feat(nvim): add colorscheme configuration options

--- config/nvim/lua/user/plugins/colorschemes.lua
+++ config/nvim/lua/user/plugins/colorschemes.lua
@@ -1,6 +1,72 @@
 return {
   { "aikow/base.nvim" },
-  { "ribru17/bamboo.nvim", lazy = true },
-  { "catppuccin/nvim", name = "catppuccin.nvim", lazy = true },
-  { "folke/tokyonight.nvim", lazy = true },
+
+  {
+    "ribru17/bamboo.nvim",
+    lazy = true,
+    opts = {
+      style = "vulgaris", -- "vulgaris" or "multiplex"
+    },
+  },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin.nvim",
+    lazy = true,
+    opts = {
+      integrations = {
+        gitsigns = true,
+        indent_blankline = {
+          enabled = true,
+          color_indent_levels = false,
+        },
+        markdown = true,
+        mason = true,
+        mini = true,
+        neotree = true,
+        cmp = true,
+        dap = true,
+        treesitter = true,
+        symbols_outline = true,
+        lsp_trouble = true,
+      },
+    },
+  },
+
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      on_highlights = function(hl, c)
+        local prompt = "2d3149"
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
+    },
+  },
 }
