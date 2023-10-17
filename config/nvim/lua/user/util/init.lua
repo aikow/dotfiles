74333c4a05a7a9cd9764c43d98624b82bcf4763a
Aiko Wessels <aiko.wessels@gmail.com>
2023-10-17 15:50:14 +0200

    feat(nvim): add injections to just files

--- config/nvim/lua/user/util/init.lua
+++ config/nvim/lua/user/util/init.lua
@@ -8,4 +8,16 @@ M.feedkeys = function(keys)
   )
 end
 
+---Take a buffer as input and return the path, and won't fail for buffers like
+---terminal buffers.
+---@param buf_id integer
+M.buf_path = function(buf_id)
+  local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf_id })
+  if buftype == "" then
+    return vim.api.nvim_buf_get_name(buf_id)
+  else
+    return vim.fn.getcwd()
+  end
+end
+
 return M
