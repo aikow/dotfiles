3f40bc21b0bae7c82cd0e37e36efff835035f3e5
Aiko Wessels <aiko.wessels@gmail.com>
2023-11-09 15:15:35 +0100

    chore(nvim): unify callback param name

--- config/nvim/after/ftplugin/rust.lua
+++ config/nvim/after/ftplugin/rust.lua
@@ -4,13 +4,13 @@ local sql_format = require("user.util.lang.sql-format").sql_format
 vim.opt_local.colorcolumn = { 100 }
 vim.opt_local.textwidth = 100
 
-vim.api.nvim_buf_create_user_command(0, "SqlFormat", function(opts)
+vim.api.nvim_buf_create_user_command(0, "SqlFormat", function(params)
   if vim.fn.executable("sql-formatter") ~= 1 then
     vim.notify("Missing sql-formatter")
     return
   end
 
-  sql_format(0, { lang = opts.lang, trim = { 4, 4 } })
+  sql_format(0, { lang = params.lang, trim = { 4, 4 } })
 end, {
   desc = "Automatically format SQL statements in rust files",
   nargs = "*",
--- config/nvim/after/ftplugin/sql.lua
+++ config/nvim/after/ftplugin/sql.lua
@@ -2,13 +2,13 @@
 -- the `sql-formatter` binary if it can be found on the path.
 --
 -- The command takes an optional argument for the SQL dialect (default: sqlite).
-vim.api.nvim_buf_create_user_command(0, "SqlFormat", function(command)
+vim.api.nvim_buf_create_user_command(0, "SqlFormat", function(params)
   if vim.fn.executable("sql-formatter") ~= 1 then
     vim.notify("`sql-formatter` is not on PATH", vim.log.levels.WARN)
     return
   end
 
-  local lang = command.args ~= "" and command.args or "sqlite"
+  local lang = params.args ~= "" and params.args or "sqlite"
   vim.cmd("%!sql-formatter -l " .. lang)
 end, {
   force = true,
