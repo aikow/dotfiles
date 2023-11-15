4a574d7a2eeb4c8d068072970ed59d6b02f26a2d
Aiko Wessels <aiko.wessels@gmail.com>
2023-11-15 11:19:57 +0100

    feat(nvim): add nushell lsp

--- config/nvim/lua/user/plugins/lsp/servers/ltex.lua
+++ config/nvim/lua/user/plugins/lsp/servers/ltex.lua
@@ -1,35 +1,36 @@
 local M = {}
 
+local on_init = function(client)
+  local spellpath = vim.fn.expand("~/.dotfiles/config/nvim/spell/en.utf-8.add")
+  local spellfile = io.open(spellpath, "r")
+  if not spellfile then
+    return
+  end
+
+  -- Construct English dictionary by reading the spell file.
+  local dict_en = vim.split(spellfile:read("*a"), "\n", { trimempty = true })
+  table.insert(dict_en, ":" .. spellpath)
+
+  client.config.settings.ltex.dictionary = {
+    ["en-US"] = dict_en,
+  }
+
+  -- Setup an auto-command that detects when a word was added to the spell
+  -- file to reload the language server.
+  vim.api.nvim_create_autocmd("FileChangedShellPost", {
+    group = vim.api.nvim_create_augroup(
+      "LTeX update dictionary",
+      { clear = true }
+    ),
+    pattern = spellpath,
+    callback = function()
+      print("Updated spell file")
+    end,
+  })
+end
+
 M.opts = {
-  on_init = function(client)
-    local spellpath =
-      vim.fn.expand("~/.dotfiles/config/nvim/spell/en.utf-8.add")
-    local spellfile = io.open(spellpath, "r")
-    if not spellfile then
-      return
-    end
-
-    -- Construct English dictionary by reading the spell file.
-    local dict_en = vim.split(spellfile:read("*a"), "\n", { trimempty = true })
-    table.insert(dict_en, ":" .. spellpath)
-
-    client.config.settings.ltex.dictionary = {
-      ["en-US"] = dict_en,
-    }
-
-    -- Setup an auto-command that detects when a word was added to the spell
-    -- file to reload the language server.
-    vim.api.nvim_create_autocmd("FileChangedShellPost", {
-      group = vim.api.nvim_create_augroup(
-        "LTeX update dictionary",
-        { clear = true }
-      ),
-      pattern = spellpath,
-      callback = function()
-        print("Updated spell file")
-      end,
-    })
-  end,
+  on_init = on_init,
   filetypes = {
     "bib",
     -- "gitcommit",
