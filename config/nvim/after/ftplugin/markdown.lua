ed6277369b068db0d7f1f9d5523182110b778068
Aiko Wessels <aiko.wessels@gmail.com>
2023-12-05 14:35:21 +0100

    feat: switch to jetbrains mono nerd font

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -1,23 +1,41 @@
 vim.opt_local.conceallevel = 0
 
 -- Automatically format markdown tables using tabular
-vim.cmd([[
-inoremap <silent> <buffer> <Bar> <Bar><Esc>:call MarkdownTable()<CR>a
+if vim.fn.exists(":Tabularize") == 2 then
+  vim.cmd([[
+    inoremap <silent> <buffer> <Bar> <Bar><Esc>:call MarkdownTable()<CR>a
 
-function! MarkdownTable()
-  let p = '^\s*|\s.*\s|\s*$'
-  if exists(':Tabularize') && getline('.') =~# '^\s*|' && (getline(line('.')-1) =~# p || getline(line('.')+1) =~# p)
-    let column = strlen(substitute(getline('.')[0:col('.')],'[^|]','','g'))
-    let position = strlen(matchstr(getline('.')[0:col('.')],'.*|\s*\zs.*'))
-    Tabularize/|/l1
-    normal! 0
-    call search(repeat('[^|]*|',column).'\s\{-\}'.repeat('.',position),'ce',line('.'))
-  endif
-endfunction
-]])
+    function! MarkdownTable()
+      let p = '^\s*|\s.*\s|\s*$'
+      let line_nr = line('.') 
+      if getline(line_nr) =~# '^\s*|' && (getline(line_nr - 1) =~# p || getline(line_nr + 1) =~# p)
+        let prefix = getline('.')[0:col('.')]
+        let column = strlen(substitute(prefix, '[^|]', '', 'g'))
+        let position = strlen(matchstr(prefix, '.*|\s*\zs.*'))
+        Tabularize/|/l1
+        normal! 0
+        call search(repeat('[^|]*|', column).'\s\{-\}'.repeat('.', position), 'ce', line_nr)
+      endif
+    endfunction
+  ]])
+end
 
 -- Markdown previewer
-local map = vim.keymap.set
-map("n", "<localleader>r", "<cmd>MarkdownPreview<CR>", { buffer = true })
-map("n", "<localleader>s", "<cmd>MarkdownPreviewStop<CR>", { buffer = true })
-map("n", "<localleader>t", "<cmd>MarkdownPreviewToggle<CR>", { buffer = true })
+vim.keymap.set(
+  "n",
+  "<localleader>r",
+  "<cmd>MarkdownPreview<CR>",
+  { buffer = true }
+)
+vim.keymap.set(
+  "n",
+  "<localleader>s",
+  "<cmd>MarkdownPreviewStop<CR>",
+  { buffer = true }
+)
+vim.keymap.set(
+  "n",
+  "<localleader>t",
+  "<cmd>MarkdownPreviewToggle<CR>",
+  { buffer = true }
+)
