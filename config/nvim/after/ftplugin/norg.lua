4770ce0f1221fa0a3e4f7b8cf8f46418292726a9
Aiko Wessels <aiko.wessels@gmail.com>
2023-12-14 14:23:14 +0100

    chore(alacritty): migrate to toml configs

--- config/nvim/after/ftplugin/norg.lua
+++ config/nvim/after/ftplugin/norg.lua
@@ -11,6 +11,28 @@ vim.keymap.set(
   { buffer = true, desc = "neorg open menu" }
 )
 
+-- -------------------------
+-- |   Custom Highlights   |
+-- -------------------------
+local note_hl = vim.api.nvim_get_hl(0, {
+  name = "MiniHipatternsNote",
+  link = false,
+})
+local norm_hl = vim.api.nvim_get_hl(0, {
+  name = "Normal",
+  link = false,
+})
+vim.api.nvim_set_hl(
+  0,
+  "MiniHipatternsNeorgToday",
+  { fg = norm_hl.bg, bg = note_hl.fg }
+)
+vim.b.minihipatterns_config = {
+  highlighters = {
+    today = { pattern = "#today", group = "MiniHipatternsNeorgToday" },
+  },
+}
+
 -- -----------------------------
 -- |   File Type Completions   |
 -- -----------------------------
