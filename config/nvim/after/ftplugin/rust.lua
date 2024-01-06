8b21b9b9bc37b73a3298f59e5dbad322d1d51087
Aiko Wessels <aiko.wessels@gmail.com>
2024-01-06 09:17:39 +0100

    feat(nvim): switch from rust-tools to rustaceanvim

--- config/nvim/after/ftplugin/rust.lua
+++ config/nvim/after/ftplugin/rust.lua
@@ -4,6 +4,16 @@ local sql_format = require("user.util.lang.sql-format").sql_format
 vim.opt_local.colorcolumn = { 100 }
 vim.opt_local.textwidth = 100
 
+local map = vim.keymap.set
+-- stylua: ignore start
+map("n", "<localleader>d", "<cmd>RustOpenExternalDocs<CR>", { buf = 0, silent = true, desc = "rust open external docs" })
+map("n", "<localleader>t", "<cmd>RustDebuggables<CR>", { buf = 0, silent = true, desc = "rust debuggables" })
+map("n", "<localleader>r", "<cmd>RustRunnables<CR>", { buf = 0, silent = true, desc = "rust runnables" })
+map("n", "<localleader>c", "<cmd>RustOpenCargo<CR>", { buf = 0, silent = true, desc = "rust open cargo" })
+map("n", "<localleader>m", "<cmd>RustExpandMacro<CR>", { buf = 0, silent = true, desc = "rust expand macro" })
+map("n", "<localleader>a", "<cmd>RustHoverActions<CR>", { buf = 0, silent = true, desc = "rust hover actions" })
+-- stylua: ignore end
+
 vim.api.nvim_buf_create_user_command(
   0,
   "SqlFormat",
--- config/nvim/lua/user/plugins/langs/rust.lua
+++ config/nvim/lua/user/plugins/langs/rust.lua
@@ -1,20 +1,10 @@
 return {
   -- Rust
   {
-    "simrat39/rust-tools.nvim",
+    "mrcjkb/rustaceanvim",
     ft = "rust",
-    config = function()
-      local ok_rust_tools, rust_tools = pcall(require, "rust-tools")
-      if not ok_rust_tools then return end
-
-      local ok_cmp_nvim_lsp, cmp_lsp = pcall(require, "cmp_nvim_lsp")
-      if not ok_cmp_nvim_lsp then return end
-
-      local capabilities = cmp_lsp.default_capabilities(
-        vim.lsp.protocol.make_client_capabilities()
-      )
-
-      rust_tools.setup({
+    init = function()
+      vim.g.rustaceanvim = {
         tools = { -- rust-tools options
           autoSetHints = true, -- Automatically set inlay hints
           -- hover_with_actions = true, -- Show action inside the hover menu
@@ -30,29 +20,15 @@ return {
           -- on_attach = function()
           --   require("user.plugins.configs.lspconfig").mappings()
           -- end,
-          capabilities = capabilities,
           settings = {
-            -- to enable rust-analyzer settings visit:
-            -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
             ["rust-analyzer"] = {
-              -- enable clippy on save
               checkOnSave = {
                 command = "clippy",
               },
             },
           },
         },
-      })
-      local map = vim.keymap.set
-
-        -- stylua: ignore start
-        map("n", "<localleader>d", "<cmd>RustOpenExternalDocs<CR>", { silent = true, desc = "rust open external docs" })
-        map("n", "<localleader>t", "<cmd>RustDebuggables<CR>", { silent = true, desc = "rust debuggables" })
-        map("n", "<localleader>r", "<cmd>RustRunnables<CR>", { silent = true, desc = "rust runnables" })
-        map("n", "<localleader>c", "<cmd>RustOpenCargo<CR>", { silent = true, desc = "rust open cargo" })
-        map("n", "<localleader>m", "<cmd>RustExpandMacro<CR>", { silent = true, desc = "rust expand macro" })
-        map("n", "<localleader>a", "<cmd>RustHoverActions<CR>", { silent = true, desc = "rust hover actions" })
-      -- stylua: ignore end
+      }
     end,
   },
 
