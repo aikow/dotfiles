56f8b050b04997bded6b2290c39c83993eec72a5
Aiko Wessels <aiko.wessels@gmail.com>
2024-01-06 09:08:09 +0100

    fix(alacritty): use correct path methods in dot.lua

--- config/alacritty/dot.lua
+++ config/alacritty/dot.lua
@@ -6,7 +6,7 @@ if dotbot.executable("alacritty") then
 	dotbot.clean({ "~/.config/alacritty" })
 
 	-- Link config files
-	Path("alacritty.toml"):link("~/.config/alacritty/alacritty.toml", { force = true })
-	Path("alacritty/themes/gruvbox-material-dark-medium.toml"):link("current-theme.toml")
+	Path("alacritty.toml"):symlink_to("~/.config/alacritty/alacritty.toml", { force = true })
+	Path("alacritty/themes/gruvbox-material-dark-medium.toml"):symlink_to("current-theme.toml")
 	Path(string.format("~/.config/alacritty/os/%s.toml", dotbot.get_os())):link("current-os.toml")
 end
