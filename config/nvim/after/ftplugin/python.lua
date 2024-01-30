b2318a4222ab467f316fa09299c1f816fe0b09e5
Aiko Wessels <aiko.wessels@gmail.com>
2024-01-30 14:00:12 +0100

    chore(nvim): remove old references to format-sql

--- config/nvim/after/ftplugin/python.lua
+++ config/nvim/after/ftplugin/python.lua
@@ -1,5 +1,3 @@
-local sql_format = require("user.util.lang.sql-format").sql_format
-
 vim.opt_local.expandtab = true
 vim.opt_local.autoindent = true
 vim.opt_local.smarttab = true
@@ -53,41 +51,3 @@ vim.api.nvim_create_autocmd("InsertCharPre", {
     )
   end,
 })
-
--- Format all injected languages
-vim.api.nvim_buf_create_user_command(
-  0,
-  "SqlFormat",
-  ---@param params NvimCommandCallbackParams
-  function(params)
-    if vim.fn.executable("sql-formatter") ~= 1 then
-      vim.notify("Missing sql-formatter")
-      return
-    end
-
-    sql_format(0, { lang = params.args, trim = { 4, 4 } })
-  end,
-  {
-    desc = "Automatically format SQL statements in python files",
-    nargs = "?",
-    complete = function()
-      return {
-        "bigquery",
-        "db2",
-        "hive",
-        "mariadb",
-        "mysql",
-        "n1ql",
-        "plsql",
-        "postgresql",
-        "redshift",
-        "singlestoredb",
-        "spark",
-        "sql",
-        "sqlite",
-        "trino",
-        "tsql",
-      }
-    end,
-  }
-)
--- config/nvim/after/ftplugin/rust.lua
+++ config/nvim/after/ftplugin/rust.lua
@@ -1,5 +1,3 @@
-local sql_format = require("user.util.lang.sql-format").sql_format
-
 -- Set rust specific vim settings.
 vim.opt_local.colorcolumn = { 100 }
 vim.opt_local.textwidth = 100
@@ -15,40 +13,3 @@ map("n", "<localleader>c", "<cmd>RustOpenCargo<CR>", opts("rust open cargo"))
 map("n", "<localleader>m", "<cmd>RustExpandMacro<CR>", opts("rust expand macro"))
 map("n", "<localleader>a", "<cmd>RustHoverActions<CR>", opts("rust hover actions"))
 -- stylua: ignore end
-
-vim.api.nvim_buf_create_user_command(
-  0,
-  "SqlFormat",
-  ---@param params NvimCommandCallbackParams
-  function(params)
-    if vim.fn.executable("sql-formatter") ~= 1 then
-      vim.notify("Missing sql-formatter")
-      return
-    end
-
-    sql_format(0, { lang = params.args, trim = { 4, 4 } })
-  end,
-  {
-    desc = "Automatically format SQL statements in rust files",
-    nargs = "?",
-    complete = function()
-      return {
-        "bigquery",
-        "db2",
-        "hive",
-        "mariadb",
-        "mysql",
-        "n1ql",
-        "plsql",
-        "postgresql",
-        "redshift",
-        "singlestoredb",
-        "spark",
-        "sql",
-        "sqlite",
-        "trino",
-        "tsql",
-      }
-    end,
-  }
-)
