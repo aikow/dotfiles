398db0f7d6d9e174ad038fb32938ff39835f19fb
Aiko Wessels <aiko.wessels@gmail.com>
2024-09-27 08:09:39 +0200

    feat(nvim): improve python keybindings

--- config/nvim/after/ftplugin/python.lua
+++ config/nvim/after/ftplugin/python.lua
@@ -10,35 +10,6 @@ vim.opt_local.textwidth = 80
 -- Set the indent after opening parenthesis
 vim.g.pyindent_open_paren = vim.bo.shiftwidth
 
--- Shortcut to run file through interpreter.
-vim.keymap.set({ "n", "v" }, "<localleader>r", [[:!python3 %<CR>]])
-vim.keymap.set({ "n", "v" }, "<localleader>b", function()
-  vim
-    .system({
-      "black",
-      "--line-length",
-      tostring(vim.o.textwidth),
-      "--preview",
-      "--enable-unstable-feature=string_processing",
-      vim.fs.normalize(vim.api.nvim_buf_get_name(0)),
-    })
-    :wait()
-  vim.cmd.edit()
-end)
-
-vim.keymap.set(
-  { "n", "x" },
-  "<leader>rl",
-  function()
-    require("conform").format({
-      async = true,
-      lsp_fallback = true,
-      formatters = { "ruff_fix" },
-    })
-  end,
-  { desc = "Format the current buffer by fixing all lints" }
-)
-
 -- Automatically make the current string an f-string when typing `{`.
 vim.api.nvim_create_autocmd("InsertCharPre", {
   pattern = { "*.py" },
@@ -130,13 +101,6 @@ local function add_annotated()
   vim.cmd.startinsert()
 end
 
-vim.keymap.set(
-  "n",
-  "<localleader>ta",
-  add_annotated,
-  { desc = "Surround the current type annotation the cursor is in with an `Annotated` block." }
-)
-
 -- Automatically wrap multiline concatenated strings.
 local iter_query = require("user.util.treesitter").iter_query
 vim.api.nvim_buf_create_user_command(0, "StringWrap", function()
@@ -219,3 +183,60 @@ vim.api.nvim_buf_create_user_command(0, "StringWrap", function()
 end, {
   desc = "automatically wrap and format a multi-line string",
 })
+
+-- ------------------------------------------------------------------------
+-- | Keymaps
+-- ------------------------------------------------------------------------
+
+vim.keymap.set(
+  { "n", "x" },
+  "<localleader>r",
+  [[:!python3 %<CR>]],
+  { desc = "Run the file or region through the interpreter" }
+)
+vim.keymap.set(
+  "n",
+  "<localleader>l",
+  "<cmd>cexpr(system('ruff check --output-format=concise'))<cr>",
+  { desc = "populate the quickfix list with the output of 'ruff check'" }
+)
+vim.keymap.set("n", "<localleader>fb", function()
+  vim
+    .system({
+      "black",
+      "--line-length",
+      tostring(vim.o.textwidth),
+      "--preview",
+      "--enable-unstable-feature=string_processing",
+      vim.fs.normalize(vim.api.nvim_buf_get_name(0)),
+    })
+    :wait()
+  vim.cmd.edit()
+end, { desc = "format the entire buffer using black with string processing enabled" })
+
+vim.keymap.set(
+  { "n", "x" },
+  "<localleader>fr",
+  function()
+    require("conform").format({
+      async = true,
+      lsp_fallback = true,
+      formatters = { "ruff_fix" },
+    })
+  end,
+  { desc = "Format the current buffer by fixing all lints using ruff" }
+)
+
+vim.keymap.set(
+  "n",
+  "<localleader>fs",
+  "<cmd>StringWrap<cr>",
+  { desc = "format the buffer by wrapping all multi-line strings" }
+)
+
+vim.keymap.set(
+  "n",
+  "<localleader>ta",
+  add_annotated,
+  { desc = "Surround the current type annotation the cursor is in with an Annotated block" }
+)
