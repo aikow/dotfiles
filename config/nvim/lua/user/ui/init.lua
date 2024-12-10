08f97f9aa3d84980423292b2902ebfccf318d8bd
Aiko Wessels <aiko.wessels@gmail.com>
2024-12-10 17:41:27 +0100

    feat(nvim): cleanup custom tabline

--- config/nvim/lua/user/ui/init.lua
+++ config/nvim/lua/user/ui/init.lua
@@ -2,50 +2,74 @@ local protocol_icons = require("user.ui.icons").protocols
 
 local M = {}
 
+-- ------------------------------------------------------------------------
+-- | Tabpage Icon Handlers
+-- ------------------------------------------------------------------------
+M.handlers = {
+  empty = function(state)
+    if state.bufname ~= "" then return end
+    return "󰎔", "[No Name]"
+  end,
+
+  protocol_terminal = function(state)
+    if state.protocol ~= "term" then return end
+    local _, pid, cmd = state.rest:match("^(.*)//(%d+):(.*)$")
+    return protocol_icons.term, pid .. " | " .. cmd
+  end,
+
+  protocol = function(state)
+    if state.protocol == nil then return end
+    local icon = protocol_icons[state.protocol] or ("[" .. state.protocol .. "]")
+    local filename = vim.fn.fnamemodify(state.rest, ":t")
+    return icon, filename
+  end,
+
+  diffview = function(state)
+    if not vim.t[state.tabid].diffview_view_initialized then return end
+    local icon = protocol_icons.diffview
+    local filename = vim.fn.fnamemodify(state.bufname, ":t")
+    return icon, filename
+  end,
+
+  default = function(state)
+    local icon = require("mini.icons").get("file", state.bufname)
+    local filename = vim.fn.fnamemodify(state.bufname, ":t")
+    return icon, filename
+  end,
+}
+
 ---Get the display name for a buffer given its normal name.
 ---@param bufname string The name of the buffer as it returned by `bufname()` and `nvim_buf_get_name()`
 ---@return string
-function M.buf_display_name(bufname, is_active)
-  if bufname == "" then return "󰎔 [No Name]" end
-
+function M.buf_display_name(tabid, is_active)
+  -- Get the current buffer of the tab
+  local winid = vim.api.nvim_tabpage_get_win(tabid)
+  local bufid = vim.api.nvim_win_get_buf(winid)
+  local bufname = vim.api.nvim_buf_get_name(bufid)
   local protocol, rest = bufname:match("^([^:]+)://(.*)$")
-  if protocol == "term" then
-    local _, pid, cmd = rest:match("^(.*)//(%d+):(.*)$")
-    return string.format(
-      "%%#%s#%s %%#%s#%s | %s",
-      is_active and "TabLineSel" or "TabLine",
-      protocol_icons.term,
-      is_active and "TabLineSel" or "TabLine",
-      pid,
-      cmd
-    )
-  elseif protocol ~= nil then
-    local icon = protocol_icons[protocol] or ("[" .. protocol .. "]")
-    local filename = vim.fn.fnamemodify(rest, ":t")
-    return string.format(
-      "%%#%s#%s %%#%s#%s",
-      is_active and "TabLineSel" or "TabLine",
-      icon,
-      is_active and "TabLineSel" or "TabLine",
-      filename
-    )
-  else
-    local icon
-    if vim.t.diffview_view_initialized then
-      icon = protocol_icons.diffview
-    else
-      icon = require("mini.icons").get("file", bufname)
-    end
+  local state = {
+    bufid = bufid,
+    winid = winid,
+    tabid = tabid,
+    bufname = bufname,
+    protocol = protocol,
+    rest = rest,
+    is_active = is_active,
+  }
 
-    local filename = vim.fn.fnamemodify(bufname, ":t")
-    return string.format(
-      "%%#%s#%s %%#%s#%s",
-      is_active and "TabLineSel" or "TabLine",
-      icon,
-      is_active and "TabLineSel" or "TabLine",
-      filename
-    )
+  local icon, tabname
+  for _, h in pairs(M.handlers) do
+    icon, tabname = h(state)
+    if icon then break end
   end
+
+  return string.format(
+    "%%#%s#%s %%#%s#%s",
+    state.is_active and "TabLineSel" or "TabLine",
+    icon,
+    state.is_active and "TabLineSel" or "TabLine",
+    tabname
+  )
 end
 
 ---Return the tabline string.
@@ -60,13 +84,8 @@ function M.tabline()
     -- Start the tab using the tabpage ID
     s = s .. "%#" .. hl .. "#%" .. tabid .. "T"
 
-    -- Get the current buffer of the tab
-    local winid = vim.api.nvim_tabpage_get_win(tabid)
-    local bufid = vim.api.nvim_win_get_buf(winid)
-    local bufname = vim.api.nvim_buf_get_name(bufid)
-
     -- Format the name of the current buffer
-    local display_name = M.buf_display_name(bufname, is_active)
+    local display_name = M.buf_display_name(tabid, is_active)
     s = s .. string.format(" %d %s ", pos, display_name)
   end
 
