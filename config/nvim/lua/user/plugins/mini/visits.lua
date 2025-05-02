f21e72b0815c930d53f219994886c5606419c67f
Aiko Wessels <aiko.wessels@gmail.com>
2025-05-02 13:24:00 +0200

    fix(nvim): cleanup mini.visits

--- config/nvim/lua/user/plugins/mini/visits.lua
+++ config/nvim/lua/user/plugins/mini/visits.lua
@@ -1,35 +1,29 @@
-local function map_visits(lhs, rhs, desc)
-  vim.keymap.set(
-    "n",
-    "<leader>j" .. lhs,
-    type(rhs) == "string" and "<cmd>" .. rhs .. "<cr>" or rhs,
-    { desc = "mini.visits " .. desc }
-  )
-end
-
-local function make_select_path(select_global, recency_weight)
-  local visits = require("mini.visits")
-  local sort = visits.gen_sort.default({ recency_weight = recency_weight })
-  local select_opts = { sort = sort }
-  return function()
-    local cwd = select_global and "" or vim.uv.cwd()
-    visits.select_path(cwd, select_opts)
-  end
-end
-
-local function map_select(lhs, desc, ...) map_visits(lhs, make_select_path(...), desc) end
-
 local minivisits = require("mini.visits")
 minivisits.setup({})
 
-map_select("r", "select recent (cwd)", false, 1)
-map_select("R", "select recent (all)", true, 1)
-map_select("y", "select frecent (cwd)", false, 0.5)
-map_select("Y", "select frecent (all)", true, 0.5)
-map_select("f", "select frequent (cwd)", false, 0)
-map_select("F", "select frequent (all)", true, 0)
+local miniextra = require("mini.extra")
 
-map_visits("j", "lua MiniVisits.add_label()", "add label")
-map_visits("J", "lua MiniVisits.remove_label()", "remove label")
-map_visits("l", "lua MiniVisits.select_label()", "select label (cwd)")
-map_visits("L", 'lua MiniVisits.select_label("", "")', "select label (all)")
+local function visit_paths(global, recency_weight)
+  return function()
+    miniextra.pickers.visit_paths({
+      cwd = global and "" or nil,
+      recency_weight = recency_weight,
+    })
+  end
+end
+
+local function visit_labels(global)
+  return function() miniextra.pickers.visit_labels({ cwd = global and "" or nil }) end
+end
+
+vim.keymap.set("n", "<leader>jr", visit_paths(false, 1.0), { desc = "select recent (cwd)" })
+vim.keymap.set("n", "<leader>jR", visit_paths(true, 1.0), { desc = "select recent (all)" })
+vim.keymap.set("n", "<leader>jy", visit_paths(false, 0.5), { desc = "select frecent (cwd)" })
+vim.keymap.set("n", "<leader>jY", visit_paths(true, 0.5), { desc = "select frecent (all)" })
+vim.keymap.set("n", "<leader>jf", visit_paths(false, 0.0), { desc = "select frequent (cwd)" })
+vim.keymap.set("n", "<leader>jF", visit_paths(true, 0.0), { desc = "select frequent (all)" })
+
+vim.keymap.set("n", "<leader>jj", minivisits.add_label, { desc = "add label" })
+vim.keymap.set("n", "<leader>jJ", minivisits.remove_label, { desc = "remove label" })
+vim.keymap.set("n", "<leader>jl", visit_labels(false), { desc = "select label (cwd)" })
+vim.keymap.set("n", "<leader>jL", visit_labels(true), { desc = "select label (all)" })
