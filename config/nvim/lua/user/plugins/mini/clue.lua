62d6d0b5ca8921331eb358e2b3be18eb384fdd19
Aiko Wessels <aiko.wessels@gmail.com>
2025-08-15 06:28:57 +0200

    feat(nvim): add square brackets to mini.clues

--- config/nvim/lua/user/plugins/mini/clue.lua
+++ config/nvim/lua/user/plugins/mini/clue.lua
@@ -61,6 +61,7 @@ miniclue.setup({
     miniclue.gen_clues.g(),
     miniclue.gen_clues.marks(),
     miniclue.gen_clues.registers(),
+    miniclue.gen_clues.square_brackets(),
     miniclue.gen_clues.windows({ submode_resize = true }),
     miniclue.gen_clues.z(),
   },
