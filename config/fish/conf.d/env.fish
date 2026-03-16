8a3fb975941ae822366eec2488df424ac5e742a1
Aiko Wessels <aiko.wessels@gmail.com>
2026-03-16 06:55:49 +0100

    feat(bash,fish,zsh): simplify and add homebrew env

--- config/fish/conf.d/env.fish
+++ config/fish/conf.d/env.fish
@@ -13,11 +13,26 @@ set -gx VISUAL nvim
 # Man pager
 set -gx MANPAGER 'nvim +Man!'
 
+# Bat
+set -gx BAT_THEME gruvbox-dark
+
 # FZF
-if command -q fd
-    set -gx FZF_DEFAULT_COMMAND 'fd --type file --follow --hidden --exclude .git --color=always'
-    set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-else if command -q rg
-    set -gx FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
-end
-set -gx FZF_DEFAULT_OPTS '--ansi --bind ctrl-f:preview-half-page-down,ctrl-b:preview-half-page-up,ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-/:toggle-preview,ctrl-t:toggle-preview-wrap --cycle --reverse --border rounded --height 50% --min-height 20 --preview-window right,40%'
+set -gx FZF_DEFAULT_COMMAND 'fd --type file --follow --hidden --exclude .git --color=always'
+set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -gx FZF_DEFAULT_OPTS '
+    --ansi
+    --bind ctrl-f:preview-half-page-down
+    --bind ctrl-b:preview-half-page-up
+    --bind ctrl-d:half-page-down
+    --bind ctrl-u:half-page-up
+    --bind ctrl-/:toggle-preview
+    --bind ctrl-t:toggle-preview-wrap
+    --cycle
+    --reverse
+    --border rounded
+    --height 50%
+    --min-height 20
+    --preview-window right,40%,follow'
+
+# Homebrew
+set -gx HOMEBREW_NO_ENV_HINTS 1
